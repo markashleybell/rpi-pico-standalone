ebb696bb5a9912ba16b31e5e8ced2368d6872499
Mark Bell <me@markb.co.uk>
2022-12-06 07:28:32 +0000

    Battery level tweaks

--- power.py
+++ power.py
@@ -3,7 +3,7 @@ from machine import ADC, Pin
 charging = Pin('WL_GPIO2', Pin.IN)
 
 full_battery = 3.85
-empty_battery = 3.24
+empty_battery = 3.20
 
 class PowerMonitor:
     def __init__(self, wlan):
@@ -35,6 +35,9 @@ class PowerMonitor:
         voltage = self.get_vsys()
 
         percentage = 100 * ((voltage - empty_battery) / (full_battery - empty_battery))
+
+        if percentage < 0:
+            percentage = 0.00
 
         if percentage > 100:
             percentage = 100.00
